47b9ff9942d351804a7e9a5074e9d7ff29d9dde0
DS-GITH <david.tecds@gmail.com>
2025-03-27 10:15:09 -0300

    Removed the components unused, Empty template

--- WDP-Site/src/App.vue
+++ WDP-Site/src/App.vue
@@ -1,30 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+
+
+  <div class="container">
+    <h1>Página em Branco</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style lang="scss" scoped>
+$cor-primaria: #42b983;
+
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+h1 {
+  color: $cor-primaria;
+  text-align: center;
 }
 </style>
